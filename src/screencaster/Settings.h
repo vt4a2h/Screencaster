64898a3c740ad12c316c093d3e3ed9538784ad14
Vitaly Fanaskov <vt4a2h@gmail.com>
2020-08-23 20:30:04 +0200

    Update copyrights

--- src/screencaster/Settings.h
+++ src/screencaster/Settings.h
@@ -2,7 +2,7 @@
 **
 ** The MIT License
 **
-** Copyright (c) 2016 Vitaly Fanaskov (vt4a2h@gmail.com)
+** Copyright (c) 2016-2020 Vitaly Fanaskov (vt4a2h@gmail.com)
 **
 ** Permission is hereby granted, free of charge, to any person obtaining a copy
 ** of this software and associated documentation files (the "Software"), to deal
